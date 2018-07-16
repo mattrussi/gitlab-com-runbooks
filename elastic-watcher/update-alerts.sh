07d2628e14c6857dd0631456fdcb8782b6c60d62
Andrew Newdigate <andrew@gitlab.com>
2018-07-16 16:10:33 +0100

    Fail on error

--- elastic-watcher/update-alerts.sh
+++ elastic-watcher/update-alerts.sh
@@ -8,5 +8,5 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 for i in $SCRIPT_DIR/watches/*.json; do
   base_name=$(basename $i)
   name=${base_name%.json}
-  curl   -X PUT "${ES_URL}_xpack/watcher/watch/${name}" -H 'Content-Type: application/json'  --data-binary "@${i}"
+  curl --fail -X PUT "${ES_URL}_xpack/watcher/watch/${name}" -H 'Content-Type: application/json'  --data-binary "@${i}"
 done
