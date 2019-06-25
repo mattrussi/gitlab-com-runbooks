fa5877179be4ede7d0ae55a0d2e7e39ec3473f32
John T Skarbek <jskarbek@gitlab.com>
2019-06-25 08:45:03 -0400

    addressing commentary

--- alertmanager/update.sh
+++ alertmanager/update.sh
@@ -12,7 +12,10 @@ declare -r k8s_file='k8s_alertmanager.yaml'
 gcloud auth activate-service-account --key-file "${SERVICE_KEY}"
 gcloud config set project "${project}"
 
-gcloud --project "${project}" kms encrypt --location=global --keyring="${kms_keyring}" --key="${kms_key}" --ciphertext-file="${chef_file}".enc --plaintext-file="${chef_file}"
-gcloud --project "${project}" kms encrypt --location=global --keyring="${kms_keyring}" --key="${kms_key}" --ciphertext-file="${k8s_file}".enc --plaintext-file="${k8s_file}"
-gsutil cp alertmanager.yml.enc gs://"${bucket}"/"${chef_file}".enc
-gsutil cp alertmanager.yml.enc gs://"${bucket}"/"${k8s_file}".enc
+declare -r files=("${chef_file}" "${k8s_file}")
+
+for file in "${files[@]}"
+do
+  gcloud --project "${project}" kms encrypt --location=global --keyring="${kms_keyring}" --key="${kms_key}" --ciphertext-file="${file}".enc --plaintext-file="${file}"
+  gsutil cp "${file}".enc gs://"${bucket}"/"${file}".enc
+done
