35c7ac291cffc31d7a9dc20718d17a5a37f48abe
Michal Wasilewski <mwasilewski@gitlab.com>
2020-02-13 12:41:45 +0100

    initial commit
    Signed-off-by: Michal Wasilewski <mwasilewski@gitlab.com>

--- elastic/api_calls/batch/change-timefield-in-indexpatterns.sh
+++ elastic/api_calls/batch/change-timefield-in-indexpatterns.sh
@@ -3,10 +3,6 @@
 set -eufo pipefail
 IFS=$'\t\n'
 
-#for index in "${indices[@]}"; do
-#  curl -sSL -H 'Content-Type: application/json' -X POST "${ES7_URL_WITH_CREDS}/pubsub-${index}-inf-${env}/_rollover" -d "$(curl_data_close_index)"
-#done
-
 curl_data_initialize() {
   cat <<EOF
 {
