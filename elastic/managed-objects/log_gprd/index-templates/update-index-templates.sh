d144435868ae9b2e03ceb719cc6678b3a09e6e31
Michal Wasilewski <mwasilewski@gitlab.com>
2021-03-03 11:40:12 +0100

    Revert "db-benchmarking should actually go to prod"
    This reverts commit 98c657063eb5aaeefccfe8147aca156cc7abe368.

--- elastic/managed-objects/log_gprd/index-templates/update-index-templates.sh
+++ elastic/managed-objects/log_gprd/index-templates/update-index-templates.sh
@@ -6,12 +6,10 @@ source "${SCRIPT_DIR}"/../../lib/update-scripts-functions.sh
 source "${SCRIPT_DIR}"/../../indices/indices-array.sh
 template_name='log_gprd_index_template.libsonnet'
 
-envs=(gprd db-benchmarking)
-for env in "${envs[@]}"; do
-  # shellcheck disable=SC2154
-  for index in "${indices[@]}"; do
-    ES7_index-template_exec_jsonnet_and_upload_json "$template_name" "$index" "$env"
-  done
+env=gprd
+# shellcheck disable=SC2154
+for index in "${indices[@]}"; do
+  ES7_index-template_exec_jsonnet_and_upload_json "$template_name" "$index" "$env"
 done
 
 ES7_put_json "_template/"
