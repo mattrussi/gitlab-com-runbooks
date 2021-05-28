90c4b17ecc3f5a3370e13d750785f870ae143474
Bob Van Landuyt <bob@vanlanduyt.co>
2021-05-28 18:21:40 +0200

    Add tooling to periodically query Thanos
    This adds tooling to periodically query Thanos and upload the query
    results into a Google Cloud Storage bucket.
    
    The script to perform these operations is
    `scripts/perform-periodic-thanos-queries.rb`. See it's `--help` output
    for information about how to use it.
    
    The script evaluates all files in
    `periodic-thanos-queries/*.queries.jsonnet` and uses the result of
    that to build requests to perform against thanos.GitLab.net.
    
    The result of the requests is stored in the `periodic-query-results`
    directory. Using the format defined in the README.
    
    The queries are validated in jsonnet at evaluation time. This happens
    in the `test-periodic-queries` CI job. The queries are not run against
    a Prometheus server or stored anywhere at this time. So we have not
    checked if the queries are actually valid.
    
    For our own infrastructure, the CI job `publish-periodic-queries`
    added here can run on  specific runner that has access to Thanos. This
    job can upload to a GCS bucket.
    
    One query failing should not block the other queries from executing.

---
+++ spec/periodic_queries/query_spec.rb
@@ -0,0 +1,87 @@
+# frozen_string_literal: true
+require 'spec_helper'
+require_relative '../../lib/periodic_queries/query'
+
+describe PeriodicQueries::Query do
+  let(:name) { 'error_budget_availability' }
+
+  subject(:query) do
+    described_class.new(
+      'error_budget_availability',
+      {
+        'type' => 'instant',
+        'query' => 'promql',
+        'time' => '123'
+      }
+    )
+  end
+
+  describe '#params' do
+    it 'excludes the type from the passed info' do
+      expected_params = { 'query' => 'promql', 'time' => '123' }
+
+      expect(query.params).to eq(expected_params)
+    end
+  end
+
+  describe '#to_result' do
+    it 'returns an unsuccessful result when the response is missing' do
+      expected_result = {
+        name => {
+          success: false,
+          status_code: nil,
+          message: nil,
+          body: nil
+        }
+      }
+
+      expect(query.to_result).to eq(expected_result)
+    end
+  end
+
+  describe '#summary' do
+    let(:expected_text) { "#{name} (instant, params: [\"query\", \"time\"])" }
+
+    it 'returns a summary of the query state' do
+      expected = "➖ #{expected_text}"
+
+      expect(query.summary).to eq(expected)
+    end
+
+    context 'with a response' do
+      it 'includes a successful status' do
+        query.response = double(PeriodicQueries::PrometheusApi::Response, success?: true)
+
+        expected = "✔ #{expected_text}"
+
+        expect(query.summary).to eq(expected)
+      end
+
+      it 'includes a failed status' do
+        query.response = double(PeriodicQueries::PrometheusApi::Response, success?: false)
+
+        expected = "❌ #{expected_text}"
+
+        expect(query.summary).to eq(expected)
+      end
+    end
+  end
+
+  describe "#success?" do
+    it 'is false without a response' do
+      expect(query.success?).to be(false)
+    end
+
+    it 'is true with a successful response' do
+      query.response = double(PeriodicQueries::PrometheusApi::Response, success?: true)
+
+      expect(query.success?).to be(true)
+    end
+
+    it 'is false with a failed response' do
+      query.response = double(PeriodicQueries::PrometheusApi::Response, success?: false)
+
+      expect(query.success?).to be(false)
+    end
+  end
+end
