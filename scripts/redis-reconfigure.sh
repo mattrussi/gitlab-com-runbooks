6e1266a38483e85cb8ee9e70a66c61bf94ea900d
Igor Wiedler <iwiedler@gitlab.com>
2021-07-02 16:43:55 +0200

    Introduce redis reconfigure script

---
+++ scripts/redis-reconfigure.sh
@@ -0,0 +1,160 @@
+#!/bin/bash
+
+set -euo pipefail
+
+if [[ $# -ne 2 ]]; then
+  >&2 echo "usage: redis-reconfigure.sh env cluster"
+  >&2 echo ""
+  >&2 echo "  e.g. redis-reconfigure.sh gstg redis-cache"
+  >&2 echo ""
+  exit 65
+fi
+
+export gitlab_env=$1
+export gitlab_redis_cluster=$2
+
+case $gitlab_env in
+pre)
+  export gitlab_project=gitlab-pre
+  ;;
+gstg)
+  export gitlab_project=gitlab-staging-1
+  ;;
+gprd)
+  export gitlab_project=gitlab-production
+  ;;
+*)
+  >&2 echo "error: unknown environment: $gitlab_env"
+  exit 1
+  ;;
+esac
+
+export redis_cli='REDISCLI_AUTH="$(sudo grep ^requirepass /var/opt/gitlab/redis/redis.conf|cut -d" " -f2|tr -d \")" /opt/gitlab/embedded/bin/redis-cli'
+
+export hosts=$(seq -f "${gitlab_redis_cluster}-%02g-db-${gitlab_env}" 1 3)
+if [[ "$gitlab_redis_cluster" = "redis-cache" ]]; then
+  export sentinel="${gitlab_redis_cluster}-sentinel-01-db-${gitlab_env}.c.${gitlab_project}.internal"
+else
+  export sentinel="${gitlab_redis_cluster}-01-db-${gitlab_env}.c.${gitlab_project}.internal"
+fi
+
+wait_for_input() {
+  declare input
+  while read  -n 1 -p "Continue (y/n): " input && [[ $input != "y" && $input != "n" ]]; do
+    echo
+  done
+  if [[ $input != "y" ]]; then
+    echo
+    >&2 echo "error: aborting"
+    exit 1
+  fi
+  echo
+}
+
+failover_if_master() {
+  export i=$1
+  export fqdn="${gitlab_redis_cluster}-$i-db-${gitlab_env}.c.${gitlab_project}.internal"
+
+  echo "> failover_if_master $fqdn"
+  ssh $fqdn "$redis_cli role | head -n1"
+
+  # if role is master, perform failover
+  if [[ "$(ssh $fqdn "$redis_cli role | head -n1")" = "master" ]]; then
+    echo failing over
+    wait_for_input
+    ssh $sentinel "/opt/gitlab/embedded/bin/redis-cli -p 26379 sentinel failover ${gitlab_env}-${gitlab_redis_cluster}"
+  fi
+
+  # wait for master to step down and sync (expect "slave" [sic] and "connected")
+  while ! [[ "$(ssh $fqdn "$redis_cli role" | head -n1)" = "slave" ]]; do
+    echo waiting for stepdown
+    sleep 1
+  done
+  while ! [[ "$(ssh $fqdn "$redis_cli --raw role" | tail -n +4 | head -n1)" = "connected" ]]; do
+    echo waiting for sync
+    sleep 1
+  done
+
+  # wait for sentinel to ack the master change
+  while [[ "$(ssh $sentinel "/opt/gitlab/embedded/bin/redis-cli -p 26379 --raw sentinel master ${gitlab_env}-${gitlab_redis_cluster}" | grep -A1 ^ip$ | tail -n +2 | awk '{ print substr($0, length($0)-1) }')" = "$i" ]]; do
+    echo waiting for sentinel
+    sleep 1
+  done
+
+  echo "< failover_if_master $fqdn"
+}
+
+reconfigure() {
+  export i=$1
+  export fqdn="${gitlab_redis_cluster}-$i-db-${gitlab_env}.c.${gitlab_project}.internal"
+
+  echo "> reconfigure $fqdn"
+
+  # double check that we are dealing with a replica
+  echo checking role
+  ssh $fqdn "$redis_cli --no-raw role"
+
+  if [[ "$(ssh $fqdn "$redis_cli role | head -n1")" = "master" ]]; then
+    >&2 echo "error: expected $fqdn to be replica, but it was a master"
+    exit 1
+  fi
+
+  if [[ "$(ssh $fqdn "$redis_cli --raw role" | tail -n +4 | head -n1)" != "connected" ]]; then
+    >&2 echo "error: expected $fqdn to be in state connected"
+    exit 1
+  fi
+
+  # check sentinel quorum
+  echo sentinel ckquorum
+  ssh $sentinel "hostname; /opt/gitlab/embedded/bin/redis-cli -p 26379 sentinel ckquorum ${gitlab_env}-${gitlab_redis_cluster}"
+
+  if [[ "$(ssh $sentinel "/opt/gitlab/embedded/bin/redis-cli -p 26379 sentinel ckquorum ${gitlab_env}-${gitlab_redis_cluster}")" != "OK 3 usable Sentinels. Quorum and failover authorization can be reached" ]]; then
+    >&2 echo "error: sentinel quorum to be ok"
+    exit 1
+  fi
+
+  # ensure current config is as expected
+  echo config get io-threads
+  ssh $fqdn "$redis_cli config get io-threads"
+
+  # run chef-client
+  echo chef-client
+  wait_for_input
+  ssh $fqdn "sudo chef-client"
+
+  # temporarily disable rdb saving to allow for fast restart
+  echo config get save
+  ssh $fqdn "$redis_cli config get save"
+
+  echo config set save
+  ssh $fqdn "$redis_cli config set save ''"
+
+  # reconfigure
+  # this _will_ restart processes
+  echo gitlab-ctl reconfigure
+  wait_for_input
+  ssh $fqdn "sudo gitlab-ctl reconfigure"
+
+  # ensure config change took effect
+  ssh $fqdn "$redis_cli config get save"
+  ssh $fqdn "$redis_cli config get io-threads"
+
+  # check sync status
+  echo $hosts | xargs -n1 -I{} ssh "{}.c.${gitlab_project}.internal" 'hostname; '$redis_cli' role | head -n1; echo'
+
+  # check sentinel status
+  ssh $sentinel "hostname; /opt/gitlab/embedded/bin/redis-cli -p 26379 sentinel ckquorum ${gitlab_env}-${gitlab_redis_cluster}"
+
+  echo "< reconfigure $fqdn"
+}
+
+failover_if_master 01
+reconfigure 01
+echo
+
+failover_if_master 02
+reconfigure 02
+echo
+
+failover_if_master 03
+reconfigure 03
