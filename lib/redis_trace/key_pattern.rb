d9e8602fd24bf589f33d24112751daeac35d5c78
Craig Miskell <cmiskell@gitlab.com>
2021-11-07 22:25:34 +0000

    Some tweaks to the redis trace analysis

--- lib/redis_trace/key_pattern.rb
+++ lib/redis_trace/key_pattern.rb
@@ -11,8 +11,11 @@ module RedisTrace
       when 'cache'
         key = key
           .gsub(%r{^(highlighted-diff-files:merge_request_diffs/)(.+)}, '\1$PATTERN')
-          .gsub(%r{^(show_raw_controller:project|ancestor|can_be_resolved_in_ui\?|commit_count_refs/heads/master|commit_count_master|exists\?|last_commit_id_for_path|merge_request_template_names|root_ref|xcode_project\?|issue_template_names|views/shared/projects/_project|application_rate_limiter|branch_names|merged_branch_names|peek:requests|tag_names|branch_count|tag_count|commit_count|size|gitignore|rendered_readme|readme_path|license_key|contribution_guide|gitlab_ci_yml|changelog|license_blob|avatar|metrics_dashboard_paths|has_visible_content\?):(.+)}, '\1:$PATTERN')
-          .gsub(%r{^cache:gitlab:(diverging_commit_counts_|github-import/)(.+)}, 'cache:gitlab:\1$PATTERN')
+          .gsub(%r{^((cache:gitlab:)?(show_raw_controller:project|ancestor|can_be_resolved_in_ui\?|commit_count_refs/heads/master|commit_count_master|exists\?|last_commit_id_for_path|merge_request_template_names|merge_request_template_names_hash|root_ref|xcode_project\?|issue_template_names|issue_template_names_hash|views/shared/projects/_project|application_rate_limiter|branch_names|merged_branch_names|peek:requests|tag_names|branch_count|tag_count|commit_count|size|gitignore|rendered_readme|readme_path|license_key|contribution_guide|gitlab_ci_yml|changelog|license_blob|avatar|avatar_cache:v1|metrics_dashboard_paths|has_visible_content\?|Ci::ListConfigVariablesService|commit_stats|DiscussionsSerializer|has_ambiguous_refs\?|user_defined_metrics_dashboard_paths)):(.+)}, '\1:$PATTERN')
+          .gsub(%r{^cache:gitlab:(diverging_commit_counts_|github-import/|blob_content_sha|commit_count_|flipper/v1/)(.+)}, 'cache:gitlab:\1$PATTERN')
+          .gsub(%r{^cache:gitlab:projects/[0-9]+/(content|last_commits)/[0-9a-f]{40}/(.+)}, 'cache:gitlab:projects/$NUMBER/\1/$LONGHASH/$PATTERN')
+          .gsub(%r{^cache:gitlab:projects/[0-9-]+/(branches/users|projects)/(.+)}, 'cache:gitlab:projects/$NUMBER/\1/$LONGHASH/$PATTERN')
+          .gsub(/^container_repository:\{[0-9]+\}:tag:(.+)/, 'container_repository/{$NUMBER}:tag:$PATTERN')
       end
 
       # Generic replacements
@@ -23,6 +26,7 @@ module RedisTrace
         .gsub(/([0-9a-f]{32})/, '$HASH')
         .gsub(/([0-9a-f]{30})/, '$HASH')
         .gsub(/([0-9]+)/, '$NUMBER')
+        .encode("UTF-8", invalid: :replace, undef: :replace)
     end
   end
 end
