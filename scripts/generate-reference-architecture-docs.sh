ce1892e3dbae09a98a793de223b264e6f6a6ec1b
Andrew Newdigate <andrew@gitlab.com>
2022-06-07 11:24:07 +0200

    docs: add saturation types to the reference architecture documentation

--- scripts/generate-reference-architecture-docs.sh
+++ scripts/generate-reference-architecture-docs.sh
@@ -11,6 +11,33 @@ REPO_DIR=$(
 tmpdir=$(mktemp -d)
 trap 'rm -rf "${tmpdir}"' EXIT
 
+function replace_marker_section() {
+  local section_name=$1
+  local snippet_source=$2
+  local file=$3
+  local tmpoutput
+  tmpoutput=$(mktemp)
+
+  awk -v snippet_source="$snippet_source" '
+    /^<!-- MARKER:'"$section_name"': do not edit this section directly. -->$/ {
+      in_marker = 1;
+      print;
+      while ((getline line < snippet_source) > 0)
+        print line
+      close(snippet_source)
+    }
+
+    /^<!-- END_MARKER:'"$section_name"' -->$/ {
+      in_marker = 0;
+    }
+
+    // {
+      if (in_marker != 1) { print }
+    }' "$file" >"$tmpoutput"
+
+  mv "$tmpoutput" "$file"
+}
+
 function render_readme_for_dir() {
   local dir=$1
 
@@ -22,24 +49,9 @@ function render_readme_for_dir() {
     "$dir/src/docs.jsonnet" \
     -m "${tmpdir}"
 
-  awk -v readme_snippet="$tmpdir/README.snippet.md" '
-    /^<!-- MARKER: do not edit this section directly. -->$/ {
-      in_marker = 1;
-      print;
-      while ((getline line < readme_snippet) > 0)
-        print line
-      close(readme_snippet)
-    }
+  replace_marker_section "slis" "$tmpdir/README.snippet-slis.md" "$dir/README.md"
+  replace_marker_section "saturation" "$tmpdir/README.snippet-saturation.md" "$dir/README.md"
 
-    /^<!-- END_MARKER -->$/ {
-      in_marker = 0;
-    }
-
-    // {
-      if (in_marker != 1) { print }
-    }' "$dir/README.md" >"$tmpdir/README.md.tmp"
-
-  mv "$tmpdir/README.md.tmp" "$dir/README.md"
   echo "$dir/README.md"
 }
 
