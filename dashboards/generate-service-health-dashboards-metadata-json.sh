0cfcb9b42e518c9dd39b252825bec588df198179
schin1 <schin@gitlab.com>
2022-09-27 19:44:21 +0800

    Update bash according to shellcheck

--- dashboards/generate-service-health-dashboards-metadata-json.sh
+++ dashboards/generate-service-health-dashboards-metadata-json.sh
@@ -4,7 +4,7 @@ set -uo pipefail
 
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-cd "${SCRIPT_DIR}"
+cd "${SCRIPT_DIR}" || exit
 
 source "grafana-tools.lib.sh"
 
@@ -12,12 +12,12 @@ TEMPFILE="tempfile.json"
 FILE="${SCRIPT_DIR}/service_health_dashboards.json"
 
 usage() {
-    cat <<-EOF
+  cat <<-EOF
   Usage [Dh]
 
   DESCRIPTION
     This script checks the existence of any dashboards defined in
-    `dashboards/**/*.dashboard.jsonnet` files and collates the result
+    "dashboards/**/*.dashboard.jsonnet" files and collates the result
     in a JSON file
 
     GRAFANA_API_TOKEN must be set in the environment
@@ -32,17 +32,17 @@ EOF
 }
 
 while getopts ":Dh" o; do
-    case "${o}" in
-        D)
-            dry_run="true"
-            ;;
-        h)
-            usage
-            exit 0
-            ;;
-        *) ;;
+  case "${o}" in
+  D)
+    dry_run="true"
+    ;;
+  h)
+    usage
+    exit 0
+    ;;
+  *) ;;
 
-    esac
+  esac
 done
 
 shift $((OPTIND - 1))
@@ -50,42 +50,36 @@ shift $((OPTIND - 1))
 dry_run=${dry_run:-}
 
 if [[ -z $dry_run && -z ${GRAFANA_API_TOKEN:-} ]]; then
-    echo "You must set GRAFANA_API_TOKEN to use this script, or run in dry run mode"
-    usage
-    exit 1
+  echo "You must set GRAFANA_API_TOKEN to use this script, or run in dry run mode"
+  usage
+  exit 1
 fi
 
 prepare
 
 trap 'rm -rf "${TEMPFILE}"' EXIT
 
-
 find_dashboards | while read -r line; do
-basename=$(basename "$line")
-relative=${line#"./"}
-folder=${GRAFANA_FOLDER:-$(dirname "$relative")}
+  basename=$(basename "$line")
+  relative=${line#"./"}
+  folder=${GRAFANA_FOLDER:-$(dirname "$relative")}
 
-uid="${folder}-${basename%%.*}"
-response=$(call_grafana_api "https://dashboards.gitlab.net/api/dashboards/uid/$uid")
-
-if [ $? -eq 0 ]
-then
+  uid="${folder}-${basename%%.*}"
+  if response=$(call_grafana_api "https://dashboards.gitlab.net/api/dashboards/uid/$uid"); then
     url=$(echo "${response}" | jq '.meta.url' | tr -d '"')
     fullurl="https://dashboards.gitlab.net$url"
 
     if test -f "$TEMPFILE"; then
-        cat <<< $(jq ".\"$folder\" |= .+ [\"$fullurl\"]" $TEMPFILE) > $TEMPFILE
+      cat <<<"$(jq ".\"$folder\" |= .+ [\"$fullurl\"]" $TEMPFILE)" >$TEMPFILE
     else
-        echo "{\"$folder\": [\"$fullurl\"]}" > $TEMPFILE
+      echo "{\"$folder\": [\"$fullurl\"]}" >$TEMPFILE
     fi
-fi
+  fi
 done
 
-
 if [[ -n $dry_run ]]; then
-    cat $TEMPFILE | jq '.'
-    rm $TEMPFILE
+  cat $TEMPFILE | jq '.'
+  rm $TEMPFILE
 else
-    cat <<< $(jq '.' $TEMPFILE) > $FILE
+  cat <<<"$(jq '.' $TEMPFILE)" >"$FILE"
 fi
-
