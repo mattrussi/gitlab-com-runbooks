fdb3af89c865b2795228132a5e7eabde1c2d327c
Stephanie Jackson <stejacks@gitlab.com>
2023-09-14 17:14:04 -0400

    Created a file name based labelling system and mapping override.

--- scripts/generate-prometheus-crd.rb
+++ scripts/generate-prometheus-crd.rb
@@ -2,6 +2,7 @@
 # frozen_string_literal: true
 
 require 'yaml'
+require 'digest'
 
 if ARGV.empty?
   puts "Usage: ruby generate-prometheus-crd.rb <directory_path>"
@@ -14,6 +15,7 @@ shard_counter = 0
 parent_directory = File.expand_path('..', __dir__)
 rules_dir = File.join(parent_directory, ARGV[0])
 rule_files = Dir.glob(File.join(rules_dir, '**', '*.{yaml,yml}'))
+mapping_file = File.join(rules_dir, ".thanos-shard-mapping")
 
 # This is templated without namespace
 # so kubectl can target the namespace for both
@@ -37,16 +39,25 @@ rule_files.each do |rule_file|
   # Don't merge yaml if its already in CRD format
   next if source_yaml.key?('kind') && source_yaml['kind'] == 'PrometheusRule'
 
+  # Load mapping file if exists
+
+  mapping_yaml = YAML.load_file(mapping_file) if File.exist? mapping_file 
+
   # Get source filename without extension
   filename_base = File.basename(rule_file, File.extname(rule_file))
+
+  if mapping_yaml[filename_base]
+    shard_value = mapping_yaml[filename_base]
+  else
+    hash_value = Digest::SHA1.hexdigest(filename_base)
+    shard_value = hash_value.to_i(16) % (shards + 1) unless mapping_yaml[filename_base]
+  end
 
   # Create new yaml
   rule_yaml = YAML.safe_load(prometheus_rule_yaml)
   rule_yaml['spec'].merge!(source_yaml)
   rule_yaml['metadata']['name'] = filename_base.gsub('_', '-')
-  rule_yaml['metadata']['labels']['shard'] = shard_counter.to_s
-
-  shard_counter = (shard_counter + 1) % (shards + 1)
+  rule_yaml['metadata']['labels']['shard'] = shard_value.to_s
 
   # Write the merged YAML content to an output file
   File.open(rule_file, 'w') { |file| file.write(rule_yaml.to_yaml) }
