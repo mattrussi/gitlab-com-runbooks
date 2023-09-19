2b07d19ce426265ea092a3890bbb6eca02a5889f
Nick Duff <nduff@gitlab.com>
2023-09-19 17:45:11 +1200

    update generate-prometheus-crd to shard consistently to a file

--- scripts/generate-prometheus-crd.rb
+++ scripts/generate-prometheus-crd.rb
@@ -14,7 +14,18 @@ shards = 6
 parent_directory = File.expand_path('..', __dir__)
 rules_dir = File.join(parent_directory, ARGV[0])
 rule_files = Dir.glob(File.join(rules_dir, '**', '*.{yaml,yml}'))
+
+# Initialize hash in case mapping file doesn't exist
+shard_mapping = {}
+
+# Load mapping file if it exists
 mapping_file = File.join(rules_dir, ".thanos-shard-mapping")
+if File.exist?(mapping_file)
+  shard_mapping = YAML.load_file(mapping_file)
+end
+
+# Initialize the shard counter
+shard_counter = 0
 
 # This is templated without namespace
 # so kubectl can target the namespace for both
@@ -35,29 +46,32 @@ rule_files.each do |rule_file|
   # Load yaml file
   source_yaml = YAML.load_file(rule_file)
 
-  # Don't merge yaml if its already in CRD format
-  next if source_yaml.key?('kind') && source_yaml['kind'] == 'PrometheusRule'
-
-  # Load mapping file if exists
-
-  mapping_yaml = YAML.load_file(mapping_file) if File.exist? mapping_file
-
   # Get source filename without extension
   filename_base = File.basename(rule_file, File.extname(rule_file))
 
-  if !mapping_yaml.nil? && mapping_yaml[filename_base]
-    shard_value = mapping_yaml[filename_base]
-  else
-    hash_value = OpenSSL::Digest::SHA256.hexdigest(filename_base)
-    shard_value = hash_value.to_i(16) % (shards + 1)
+  # Assign a shard value or retrieve the shard value from the mapping
+  shard_value = shard_mapping.fetch(filename_base) do
+    shard_value = shard_counter
+    shard_mapping[filename_base] = shard_value
+    shard_counter = (shard_counter + 1) % (shards + 1)
+    shard_value
   end
 
-  # Create new yaml
+  puts "Shard Counter: #{shard_counter}"
+
   rule_yaml = YAML.safe_load(prometheus_rule_yaml)
-  rule_yaml['spec'].merge!(source_yaml)
+
+  # Don't merge yaml if its already in CRD format
+  unless source_yaml.key?('kind') && source_yaml['kind'] == 'PrometheusRule'
+    rule_yaml['spec'].merge!(source_yaml)
+  end
+  
   rule_yaml['metadata']['name'] = filename_base.gsub('_', '-')
   rule_yaml['metadata']['labels']['shard'] = shard_value.to_s
 
   # Write the merged YAML content to an output file
   File.open(rule_file, 'w') { |file| file.write(rule_yaml.to_yaml) }
 end
+
+# Saving mapping
+File.open(mapping_file, 'w') { |file| file.write(shard_mapping.to_yaml) }
